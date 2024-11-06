6f4d8a7fcf9dcaebf43fd11673c68c8eafb733d7
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2024-11-05 19:37:44 -0500

    Update root.tsx

--- remix-tut/app/root.tsx
+++ remix-tut/app/root.tsx
@@ -1,5 +1,6 @@
 import type { LinksFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
+import { createEmptyContact, getContacts } from "./data";
 
 import {
   Form,
@@ -21,6 +22,11 @@ export const links: LinksFunction = () => [
 export const loader = async () => {
   const contacts = await getContacts();
   return json({ contacts });
+};
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
 };
 
 export default function App() {
