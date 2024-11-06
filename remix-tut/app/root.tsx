5db4a8bf7a80af55b469a50834ac351f658b9c9c
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2024-11-05 19:30:01 -0500

    Update root.tsx

--- remix-tut/app/root.tsx
+++ remix-tut/app/root.tsx
@@ -3,6 +3,7 @@ import {
   Form,
   Links,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
@@ -48,6 +49,9 @@ export default function App() {
               </li>
             </ul>
           </nav>
+        </div>
+        <div id="detail">
+          <Outlet />
         </div>
 
         <ScrollRestoration />
