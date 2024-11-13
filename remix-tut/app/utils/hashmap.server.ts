47cb19f447c4f3067993b6b6d40d8c0ec48dd030
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2024-11-12 19:13:20 -0500

    fix: fix type

--- remix-tut/app/utils/hashmap.server.ts
+++ remix-tut/app/utils/hashmap.server.ts
@@ -39,28 +39,29 @@ export default class HashMap {
     token: string,
     magnet: string,
     expiry: number = 60 * 60
-  ): Promise<void> {
-    if (!redis) return;
+  ): Promise<void | null> {
+    console.info("Redis health:", (await this.getKeysCnt()) / 10 ** 6);
+    if (!redis) return null;
     await redis.hset("tokenMap", token, magnet);
     await redis.expire("tokenMap", expiry);
   }
 
   static async get(token: string): Promise<string | null> {
-    if (!redis) return;
+    if (!redis) return null;
     return await redis.hget("tokenMap", token);
   }
 
-  static async getKeysCnt(): Promise<number> {
-    if (!redis) return;
+  static async getKeysCnt(): Promise<number | null> {
+    if (!redis) return null;
     return await redis.hlen("tokenMap");
   }
 
-  static async del(token: string): Promise<void> {
-    if (!redis) return;
+  static async del(token: string): Promise<void | null> {
+    if (!redis) return null;
     await redis.hdel("tokenMap", token);
   }
 
-  static async genToken(magnet: string): Promise<string> {
+  static async genToken(magnet: string): Promise<string | null> {
     if (!redis || !magnet) return "";
     const token_num = await HashMap.genKey(magnet);
     const token_str = token_num.toString().padStart(6, "0");
