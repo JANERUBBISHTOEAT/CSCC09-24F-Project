63c1e7130e87c0339ef3237276a957dc5bfa849a
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2024-11-21 14:45:10 -0500

    delete unused files

--- remix-tut/app/routes/files.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "@remix-run/node";
-
-export const loader = () => {
-  return redirect("/?message=Page+Not+Found");
-};
-
-export default function Files() {
-  return null;
-}
