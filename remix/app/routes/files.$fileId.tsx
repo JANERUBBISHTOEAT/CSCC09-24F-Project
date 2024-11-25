9c84f26d9d2c3c839c87857602c914d4d03c7325
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2024-11-24 20:11:00 -0500

    fix: Missing other files icon

--- remix/app/routes/files.$fileId.tsx
+++ remix/app/routes/files.$fileId.tsx
@@ -54,8 +54,9 @@ export default function File() {
           key={file.magnet}
           // src={file.magnet}
           className={
-            (file ? fileIconMap[file.type] : "fas fa-file-question") +
-            " fa-2x file-icon"
+            (file && file.type
+              ? fileIconMap[file.type]
+              : "fas fa-file-question") + " fa-2x file-icon"
           }
         ></i>
       </div>
